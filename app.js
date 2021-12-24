9b69af1f5aff0176649ce0e6efd957e129261123
Sourav Chakraborty <91521433+Shawebit@users.noreply.github.com>
2021-12-24 17:06:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const path = require('path');
 const app = express();
 app.use(express.json());
 app.use(express.static("express"));
+
 // default URL for website
 app.use('/', function(req,res){
     res.sendFile(path.join(__dirname+'/index.html'));
-    //dirname : It will resolve to your project folder.
   });
 const server = http.createServer(app);
-const port = 3000;
+const port = process.env.PORT || 1311;
 server.listen(port);
 console.debug('Server listening on port ' + port);
